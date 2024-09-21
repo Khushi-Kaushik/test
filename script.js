9dae57df30801ecf994adc467fbd8e98f58c91d8
Khushi-Kaushik <khushikaushik@gmail.com>
2024-09-21 20:50:33 +0530

    Page 1 updated

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ g.from(".bottom",{
 g.to(".loader",{
     opacity:0,
     duration:0.4,
-    delay:1.5
+    delay:1
 })
 g.from(".page1",{
     delay:0.5,
@@ -48,12 +48,62 @@ g.from(".page1",{
 g.to(".loader",{
     display:"none"
 })
-}
-loaderPage()
-g.from(".page1",{
-    y:150,
-    stagger:0.1,
-    duration:0.4,
-    delay:0.4
+g.from(".nav",{
+    
+    opacity:0
 })
+g.from(".number h1",{
+    duration: 1.1,
+        opacity: 0,
+        y: 50,
+        ease: "power3.in",
+})
+g.from(".hero h1",{
+    y:120,
+    stagger:0.1,
+    opacity:0
+})
+g.from(".hero h2",{
+    y:120,
+    stagger:0.1,
+    opacity:0
+})
+}
 
+function cursoranimation(){
+    document.addEventListener("mousemove",function(move){
+        gsap.to(".crsr",{
+            left:move.x,
+            top:move.y
+        })
+    })
+    Shery.makeMagnet(".nav-2 h3" , {
+        // ease: "cubic-bezier(1, 5, 1, 2)",
+        // duration: 0.5,
+      });
+    Shery.makeMagnet(".menu-opener__square" , {
+        // ease: "cubic-bezier(1, 2, 1, 2)",
+        // duration: 0.5,
+    });
+}
+
+
+
+cursoranimation()
+loaderPage()
+
+gsap.to(".bottom-left h3", {
+    y: -100, // Move text up
+    opacity: 0, // Fade out
+    duration: 2, // Duration of the animation
+    delay:1,
+    ease: "power1.in", // Ease in
+    repeat: -1, // Infinite loop
+    yoyo: true, // Move back and forth
+    repeatDelay: 0.5, // Delay between repeats
+    onRepeat: () => {
+      // Bring text back to the bottom once it's invisible
+      gsap.set(text, { y: 100, opacity: 0 });
+      gsap.to(text, { y: 0, opacity: 1, duration: 1.5, ease: "power1.out" });
+    }
+  });
